f6b578cda11c40393e025866f20b864bf7275bb5
Rohan Arora <rohan@myMac.local>
2020-04-29 17:50:27 +0530

    Implemented f1 case

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -9,6 +9,45 @@ use Illuminate\Support\Facades\Http;
 class EventsController extends Controller
 {
     protected $case1 = ['2', '74', '147', '9806', '321', '10548', '10488'];
+    protected $similarEvents = [
+        '74' => ['2', '414', '412', '413']
+    ];
+    protected $performers = [
+        '74' => [
+            [
+                "name" => "Lewis Hamilton",
+                "img_src" => "1.jpg"
+            ],
+            [
+                "name" => "valtteri bottas",
+                "img_src" => "2.jpg"
+            ],
+            [
+                "name" => "sebastian vettel",
+                "img_src" => "3.jpg"
+            ],
+            [
+                "name" => "charles leclerc",
+                "img_src" => "4.jpg"
+            ],
+            [
+                "name" => "max verstappen",
+                "img_src" => "5.jpg"
+            ],
+            [
+                "name" => "alexander albon",
+                "img_src" => "6.jpg"
+            ],
+            [
+                "name" => "daniel ricciardo",
+                "img_src" => "7.jpg"
+            ],
+            [
+                "name" => "esteban ocon",
+                "img_src" => "8.jpg"
+            ]
+        ]
+    ];
 
     public function index()
     {
@@ -24,6 +63,18 @@ class EventsController extends Controller
         $eventUrl = 'https://eventraveler.com/get_event?event_id='.$eventId;
         $response = Http::get($eventUrl);
         $event = $response->json()[0];
-        return view('event_detail', ['event' => $event]);
+
+        $eventUrl = 'https://eventraveler.com/get_events?ids='.$eventId;
+        $response = Http::get($eventUrl);
+        $eventinfo = $response->json()[0];
+
+        $similarEventIds = $this->similarEvents[$eventId];
+        $similarEventsUrl = 'https://eventraveler.com/get_events?ids='.implode(",", $similarEventIds);
+        $similarEventsResponse = Http::get($similarEventsUrl);
+        $similarEvents = $similarEventsResponse->json();
+
+        $performers = !empty($this->performers[$eventId]) ? $this->performers[$eventId] : [];
+
+        return view('event_detail', ['performers' => $performers, 'info' => $eventinfo, 'event' => $event, 'similar_events'=> $similarEvents]);
     }
 }
--- resources/views/event_detail.blade.php
+++ resources/views/event_detail.blade.php
@@ -15,6 +15,16 @@
 @endsection
 
 @section('body')
+
+    @php
+        $externalImage = strpos($info['img_src'], 'ttps:');
+        $imgSrc = $info['img_src'];
+        if(!$externalImage)
+        {
+               $imgSrc = 'https://s3.us-east-2.amazonaws.com/eventraveler/'.$info['img_src'];
+        }
+    @endphp
+
     <div class="container-fluid">
         <div class="row">
 
@@ -25,25 +35,27 @@
                     <div class="shadow-none no-border-radium card card-cascade wider reverse">
 
                         <!-- Card image -->
-                        <div class="view view-cascade overlay" style="max-height: 300px;overflow: hidden">
-                            <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" alt="Card image cap">
+                        <div class="view view-cascade overlay" style="background-image:url('{{$imgSrc}}'); background-position:center; background-size:cover;height: 300px;overflow: hidden">
                         </div>
 
                         <!-- Card content -->
                         <div class="container">
                         <div class="col-12">
-                            <div class="shadow-none  event-detail-card text-center event-search-form">
+                            <div class="shadow-none  event-detail-card text-center event-search-form px-0">
 
                                 <!-- Title -->
-                                <span class="fs-16 color-orange pb-1 d-block">Mon 1st to Sat 7th April, 2020</span>
-                                <h4 class="font-weight-bold text-dark mb-0">Tomorrowland 2020</h4>
+                                <span class="fs-16 color-orange pb-1 d-block">
+                                    {{\App\Helpers\EventDateHelper::printStartEndDate($event['s_date'], $event['e_date'])}}
+                                </span>
+                                <h4 class="font-weight-bold text-dark mb-0">{{$event['name']}}</h4>
                                 <!-- Subtitle -->
-                                <h6 class="py-2 fs-16 text-gray"><span class="flag-icon flag-icon-be fs-18 mr-2"></span>
-                                    Boom, Belgium</h6>
+                                <h6 class="py-2 fs-16 text-gray"><span class="flag-icon flag-icon-{{strtolower($info['iso2'])}} fs-18 mr-2"></span>
+                                    {{$info['city_name']}}, {{$info['short_name']}}
+                                </h6>
                                 <hr>
                                 <!-- Text -->
                                 <div class="card-text fs-18 col-12 p-0">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem perspiciatis voluptatum a, quo nobis, non commodi quia repellendus sequi nulla voluptatem dicta reprehenderit, placeat laborum ut beatae ullam suscipit veniam.
+                                    {!! $event['short_desc'] !!}
                                 </div>
                                 <div class="bg-light d-inline-block shadow-sm mt-3 p-3">
                                     <strong>Tickets starting from $44</strong>
@@ -54,27 +66,28 @@
                                     <a href="" class="mt-1 mb-2 btn btn-outline-primary font-weight-bold"><i class="fas fa-info-circle"></i>&nbsp;Learn More</a>
                                 </div>
 
+                                @if(count($performers))
                                 <div class="col-12 mt-5">
                                     <h5 class="font-weight-bold text-dark">Performers</h5>
                                 </div>
 
                                 <div class="col-12 col-lg-10 d-inline-block m-auto pt-3">
                                     <div class="row">
-                                        @for($i = 0; $i <= 7; $i ++)
+                                        @foreach($performers as $performer)
                                         <div class="col-4 col-lg-3 p-1">
                                             <div class="col-12 p-0 shadow-sm">
-                                                <div class="col-12 p-3 performer" style="background-image: url('https://media.resources.festicket.com/image/696x800/smart/filters:quality(70)/www/artists/LEWIS_CAPALDI_Leadshot_RGB.jpg')">
+                                                <div class="col-12 p-3 performer" style="background-position:top; background-image: url('/assets/images/f1/{{$performer['img_src']}}')">
 
                                                 </div>
                                                 <div class="col-12 px-0 py-2 font-weight-bold">
-                                                    Test
+                                                    {{ucwords($performer['name'])}}
                                                 </div>
                                             </div>
                                         </div>
-                                        @endfor
+                                        @endforeach
                                     </div>
-
                                 </div>
+                                @endif
 
                             </div>
                         </div>
@@ -91,7 +104,7 @@
 
     <div class="container animated fadeInUp fast">
         <div class="col-12 mt-4">
-            <h5 class="font-weight-bold">6 similar events found!</h5>
+            <h5 class="font-weight-bold">{{count($similar_events)}} similar events found!</h5>
             <hr>
         </div>
     </div>
@@ -99,8 +112,8 @@
     <div class="container">
         <div class="col-12">
             <div class="row">
-                @isset($events)
-                @foreach($events as $event)
+                @isset($similar_events)
+                @foreach($similar_events as $event)
                     @include('includes.event_card')
                 @endforeach
                 @endisset
